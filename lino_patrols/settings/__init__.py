4bb03dbe680133abf2e3ae7c2e644fdb88b62974
Luc Saffre <luc.saffre@gmail.com>
2015-07-16 17:01:47 +0300

    http://luc.lino-framework.org/blog/2015/0716.html

--- lino_patrols/settings/__init__.py
+++ lino_patrols/settings/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-## Copyright 2013 Luc Saffre
+## Copyright 2013-2015 Luc Saffre
 ## This file is part of the Lino project.
 ## Lino is free software; you can redistribute it and/or modify 
 ## it under the terms of the GNU General Public License as published by
@@ -48,7 +48,6 @@ class Site(Site):
         yield super(Site, self).get_installed_apps()
             
         yield 'lino.modlib.contenttypes'
-        yield 'lino.modlib.system'
         yield 'lino.modlib.users'
         yield 'lino.modlib.changes'
         yield 'lino.modlib.countries'
