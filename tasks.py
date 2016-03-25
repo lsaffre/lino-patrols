4f0e2519132afff83fd17281e9f3b17d3fd4bf3d
Luc Saffre <luc.saffre@gmail.com>
2016-03-25 20:18:32 +0200

    http://luc.lino-framework.org/blog/2016/0325.html

--- tasks.py
+++ tasks.py
@@ -1,6 +1,4 @@
-from atelier.tasks import ns, setup_from_tasks
-
-setup_from_tasks(globals(), "lino_patrols")
-
+from atelier.tasks import ns
+ns.setup_from_tasks(globals(), "lino_patrols")
 ns.configure({})
 # ns.configure(dict(languages="en de fr et".split()))
