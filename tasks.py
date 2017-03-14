93b69b81845d665380d72d104ec4aefc99576670
Luc Saffre <luc.saffre@gmail.com>
2017-03-14 22:26:32 +0200

    http://luc.lino-framework.org/blog/2017/0314.html

--- tasks.py
+++ tasks.py
@@ -1,4 +1,8 @@
 from lino.invlib.ns import ns
-ns.setup_from_tasks(globals(), "lino_patrols")
-ns.configure({})
-# ns.configure(dict(languages="en de fr et".split()))
+ns.setup_from_tasks(
+    globals(), "lino_patrols",
+    revision_control_system='git',
+    tolerate_sphinx_warnings=False,
+    blogref_url='http://luc.lino-framework.org',
+    cleanable_files=['docs/api/lino_patrols.*'],
+    demo_projects=['lino_patrols.settings.demo'])
