abecea70a5943293da4346e48316b7e7354391ea
Luc Saffre <luc.saffre@gmail.com>
2017-09-20 15:15:26 +0300

    http://luc.lino-framework.org/blog/2017/0920.html

--- tasks.py
+++ tasks.py
@@ -1,8 +1,8 @@
-from lino.invlib.ns import ns
+from atelier.invlib.ns import ns
 ns.setup_from_tasks(
     globals(), "lino_patrols",
     revision_control_system='git',
     tolerate_sphinx_warnings=False,
     blogref_url='http://luc.lino-framework.org',
     cleanable_files=['docs/api/lino_patrols.*'],
-    demo_projects=['lino_patrols.settings.demo'])
+    demo_projects=['.'])
