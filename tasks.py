c96d8a5f43e988ac27aa69d01632e12849597b00
Luc Saffre <luc.saffre@gmail.com>
2021-04-25 19:12:01 +0300

    fix redirected urls

--- tasks.py
+++ tasks.py
@@ -3,6 +3,6 @@ ns = setup_from_tasks(
     globals(), "lino_patrols",
     revision_control_system='git',
     tolerate_sphinx_warnings=False,
-    blogref_url='http://luc.lino-framework.org',
+    blogref_url='https://luc.lino-framework.org',
     cleanable_files=['docs/api/lino_patrols.*'],
     demo_projects=['lino_patrols.demo'])
